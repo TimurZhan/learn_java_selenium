1a07fa46cfb23124ddec45dc2d4ad669b972f91a
Timysh <strategtim@gmail.com>
2016-11-23 14:46:01 +0800

    Переделка функции hello

--- testProject/src/main/java/ru/stqa/pft/testProject/MyFirstProgram.java
+++ testProject/src/main/java/ru/stqa/pft/testProject/MyFirstProgram.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.testProject;
 public class MyFirstProgram{
 
 	public static void main(String[] args) {
-		hello();
+		hello("world");
+		hello("Tom");
+		hello("QA");
 
 		Square m = new Square(15); // Создан новый объект m (экземпляр класса), в который передается класс (объект) типа Square со значением.
 		System.out.println("Площадь квадрата  со стороной " + m.n + " = " +  m.area()); // Значение объекта m передается ф-ии area, в качестве параметра.
@@ -12,8 +14,8 @@ public class MyFirstProgram{
 		System.out.println("Площадь треугольника со сторонами " + v.a + " и " + v.b + " = " + v.area());
 	}
 
-	public static void hello() {
-		System.out.println("Hello, world!");
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
 	}
 
 }
