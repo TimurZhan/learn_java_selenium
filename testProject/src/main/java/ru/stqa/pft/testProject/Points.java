863e8755b72e04e86c63ed51a62b608945bfa417
Timysh <strategtim@gmail.com>
2016-11-24 22:57:47 +0800

    Доработка класса Point

--- testProject/src/main/java/ru/stqa/pft/testProject/Points.java
+++ testProject/src/main/java/ru/stqa/pft/testProject/Points.java
@@ -4,19 +4,8 @@ public class Points {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x1 = 5;
-    p1.x2 = 10;
-    p2.y1 = 5;
-    p2.y2 = 10;
-
-    System.out.println("Расстояние между точками X и Y = " + distance(p1, p2));
-  }
-
-  public static double distance(Point p1, Point p2) {
-
-    return Math.sqrt(((p1.x2 - p1.x1)*(p1.x2 - p1.x1)) + ((p2.y2 - p2.y1)*(p2.y2 - p2.y1)));
+    Point p = new Point(6, 8, 3, 9);
+    System.out.println("Расстояние между точками X и Y = " + p.distance());
 
   }
 
