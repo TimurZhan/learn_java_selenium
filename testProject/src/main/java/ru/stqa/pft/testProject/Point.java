3aaa0936491dd3e2bd81fe304b2a878a724273f7
Timysh <strategtim@gmail.com>
2016-11-24 22:41:12 +0800

    Создан класс Point  и функция distance для представления точек на двумерной плоскости.

---
+++ testProject/src/main/java/ru/stqa/pft/testProject/Point.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.testProject;
+
+/**
+ * Created by Tim on 24.11.2016.
+ */
+public class Point {
+
+    public double x1;
+    public double x2;
+    public double y1;
+    public double y2;
+
+}
