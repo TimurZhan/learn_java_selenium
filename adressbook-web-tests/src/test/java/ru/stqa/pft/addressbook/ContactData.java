9b3505266f31dda8fce393e24713b586b9fdba85
Timysh <strategtim@gmail.com>
2016-11-28 15:01:23 +0800

    Написан тест для создания контакта

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,56 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String companyName;
+  private final String address1;
+  private final String mobilePhoneNumber;
+  private final String email;
+  private final String address2;
+
+  public ContactData(String firstname, String middlename, String lastname, String companyName,
+                     String address1, String mobilePhoneNumber, String email, String address2) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.companyName = companyName;
+    this.address1 = address1;
+    this.mobilePhoneNumber = mobilePhoneNumber;
+    this.email = email;
+    this.address2 = address2;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAddress1() {
+    return address1;
+  }
+
+  public String getMobilePhoneNumber() {
+    return mobilePhoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+}
