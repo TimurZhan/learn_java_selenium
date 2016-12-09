a454e614116ebe417b4f1ae61434efb21d7ec94b
Timysh <strategtim@gmail.com>
2016-12-09 20:59:02 +0800

    Оптимизирован переход по страницам.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -15,10 +15,18 @@ public class NavigationHelper extends HelperBase {
 
   //Создан отдельный вспомогательный метод для шага перехода в раздел "Группы".
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))// Создана конструкция "if", которая проверяет дислоцирование на странице "Groups"
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+      return;
+    }
+      click(By.linkText("groups"));
   }
 
   public void returnToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home page"));
   }
 
