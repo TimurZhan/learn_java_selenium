7f485349dbd907b15a23e6955a0b5442557f409a
Timysh <strategtim@gmail.com>
2016-12-30 17:32:33 +0800

    Реализована проверка телефонов для контактов
    Реализована разбивка строки на отдельные строки. А также  реализован
    метод для замены некоторых символов (для корректного сравнения номеров
    телефонов). Для этого задействованны РЕГУЛЯРНЫЕ ВЫРАЖЕНИЯ.  Когда строка
    склеивается в единое целое. Пример, есть телефон: 8 (905) 456-74-56,
    после проверки РВ телефон будет: 89054567456

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactPhoneTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/ContactPhoneTests.java
@@ -1,9 +1,11 @@
 package ru.stqa.pft.addressbook.test;
 
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactPhoneTests extends TestBase {
 
@@ -13,14 +15,10 @@ public class ContactPhoneTests extends TestBase {
     if (app.contact().all().size() == 0){//Создана проверка предусловия того, что удаляемая группа существует.
       app.contact().create(new ContactData()
               .withFirstname("sdfTest")
-              .withMiddlename("Tsdfsdfesу2")
               .withLastname("Test3")
-              .withCompanyName("Test Company")
-              .withAddress1("Test address")
-              .withMobilePhoneNumber("8900045001")
-              .withEmail("test1test3test2@mail.ru")
-              .withAddress2("Test address 2")
-              .withGroup("Тест 2222"), true);
+              .withHomePhoneNumber("90534535")
+              .withWorkPhoneNumber("345345")
+              .withMobilePhoneNumber("9899999"), true);
     }
   }
 
@@ -30,5 +28,15 @@ public class ContactPhoneTests extends TestBase {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditFrom = app.contact().InfoFromEditFrom(contact);
 
+    assertThat(contact.getHomePhoneNumber(), equalTo(cleaned(contactInfoFromEditFrom.getHomePhoneNumber())));//Сверяем телефон из поля Home
+    assertThat(contact.getMobilePhoneNumber(), equalTo(cleaned(contactInfoFromEditFrom.getMobilePhoneNumber())));//Сверяем телефон из поля Mobile
+    assertThat(contact.getWorkPhoneNumber(), equalTo(cleaned(contactInfoFromEditFrom.getWorkPhoneNumber())));//Сверяем телефон из поля Work
+  }
+
+  //Реализован метод для замены некоторых символов (для корректного сравнения номеров телефонов).
+  //Для этого задействованны РЕГУЛЯРНЫЕ ВЫРАЖЕНИЯ, которые убирают перечисленные в шаблонах символы ("[-()]", "") и пробелы ("\\s", "").
+  //А вместо них строка склеивается в единое целое. Пример, есть телефон: 8 (905) 456-74-56, после проверки РВ телефон будет: 89054567456
+  public String cleaned(String phone){
+    return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
 }
