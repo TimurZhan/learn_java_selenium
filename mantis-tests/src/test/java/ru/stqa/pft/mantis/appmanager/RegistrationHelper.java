882eef5ec8e2af9a0459e5de6e2d975b70dd3176
Timysh <strategtim@gmail.com>
2017-01-28 20:44:08 +0800

    Создан тест проверки регистрации нового юзера
    Реализована возможность получения почтового сообщения с  почтового
    сервера, извлечение из него ссылки, по которой новый юзер может
    зарегистрироваться .

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+    super(app);
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    type(By.name("username"), username); //Тут заполняется поле username
+    type(By.name("email"), email); //Тут заполняется поле email
+    click(By.cssSelector("input[value='Signup']")); //Тут нажимается кнопка Signup
+  }
+
+  public void finish(String s, String password) {
+    wd.get(s); //Тут проходим по ссылке
+    type(By.name("password"), password); //Заполняем поле
+    type(By.name("password_confirm"), password); //Заполняем поле
+    click(By.cssSelector("input[value='Update User']")); //Тут нажимается кнопка
   }
 }
