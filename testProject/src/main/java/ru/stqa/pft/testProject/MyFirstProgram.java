7dbe8b1dd5d88c2e52adbf91e05a9596ef3bc3e0
Timysh <strategtim@gmail.com>
2017-02-11 15:02:35 +0800

    rer

--- testProject/src/main/java/ru/stqa/pft/testProject/MyFirstProgram.java
+++ testProject/src/main/java/ru/stqa/pft/testProject/MyFirstProgram.java
@@ -8,10 +8,10 @@ public class MyFirstProgram{
 		hello("QA");
 
 		Square m = new Square(15); // Создан новый объект m (экземпляр класса), в который передается класс (объект) типа Square со значением.
-		System.out.println("Площадь квадрата  со стороной " + m.n + " = " +  m.area()); // Значение объекта m передается ф-ии area, в качестве параметра.
+		System.out.println("plochad kvadrata = " + m.n + " = " +  m.area()); // Значение объекта m передается ф-ии area, в качестве параметра.
 
 		Rectangle v = new Rectangle(6, 13);
-		System.out.println("Площадь треугольника со сторонами " + v.a + " и " + v.b + " = " + v.area());
+		System.out.println("plochad treygolnika = " + v.a + " and " + v.b + " = " + v.area());
 	}
 
 	public static void hello(String somebody) {
--- testProject/src/main/java/ru/stqa/pft/testProject/Points.java
+++ testProject/src/main/java/ru/stqa/pft/testProject/Points.java
@@ -5,7 +5,7 @@ public class Points {
   public static void main(String[] args) {
 
     Point p = new Point(6, 8, 3, 9);
-    System.out.println("Расстояние между точками X и Y = " + p.distance());
+    System.out.println("rasstoyanie mezhdu X and Y = " + p.distance());
 
   }
 
